7d944d7a4e0f92ddbe82b170b642794625afc162
sh3ki <shekaigarcia@gmail.com>
2026-01-12 02:47:10 +0800

    Create delete_user.php

---
+++ delete_user.php
@@ -0,0 +1,29 @@
+<?php 
+    session_start();
+    include ('../kusso/includes/header.php');
+    include('../kusso/includes/navbar.php');
+    include('../kusso/includes/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>KUSSO-Users</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+
+    <?php 
+        include ('../kusso/includes/footer.php');
+        include ('../kusso/includes/scripts.php');
+     ?>         
+
+  </body>  
+</html>              
