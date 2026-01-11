6a7f22561d50db4cbd7705953f199a235be09363
sh3ki <shekaigarcia@gmail.com>
2026-01-12 02:47:12 +0800

    Create includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+    </div>
+    <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+            <div class="text-center small">
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
